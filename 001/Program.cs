b858dbffbf2398fe0e6139a7ee9a412641f82552
Vasiliy <vapalados@gmail.com>
2022-09-17 11:51:04 +0700

    Сделаны 4 задания

---
+++ 001/Program.cs
@@ -0,0 +1,19 @@
+﻿ //Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число!");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+Console.WriteLine($"Имеем два числа {a} и {b}!");
+
+if
+(a > b)
+{
+     Console.WriteLine($"Максимальное число: {a} , минимальное число: {b}!");
+}
+
+else 
+{
+    Console.WriteLine($"Максимальное число: {b}, минимальное число: {a}!");
+}
---
+++ 002/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число!");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число!");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число!");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Имеем три числа {a}, {b} и {c}!");
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+Console.Write("Максимальное = ");
+Console.WriteLine(max);
